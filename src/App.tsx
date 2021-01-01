beb87dc543e801eef5027bc2d37b4407ec25fb08
Kassam Ali Asif <asif.k.dev@gmail.com>
2021-01-01 18:21:29 +0100

    improvement function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import { 
   IonApp, 
   IonHeader, 
@@ -7,8 +7,16 @@ import {
   IonContent,
   IonItem,
   IonLabel,
-  IonInput
+  IonInput,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonIcon
 } from '@ionic/react';
+
+// Import of Ionic Icons :
+import { calculator, refreshCircleOutline } from 'ionicons/icons'; 
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -29,7 +37,34 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+
+  const heightInputRef = useRef<HTMLIonInputElement>(null);
+  const weightInputRef = useRef<HTMLIonInputElement>(null);
+
+  const calculateBMI = () => {
+    // current? : vérifie si la propriété n'est pas null 
+    const enteredHeight = heightInputRef.current ? heightInputRef.current.value : null ;
+    const enteredWeight = weightInputRef.current?.value;
+
+    if(!enteredHeight || !enteredWeight) {
+      return;
+    }
+
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight);
+
+    console.log(bmi)
+
+  }
+
+  const resetInputs = () => {
+    // current! : on garantit que la valeur ne sera jamais null 
+    heightInputRef.current!.value = '';
+    weightInputRef.current!.value = '';
+
+  }
+
+  return(
   <IonApp>
     <IonHeader>
       <IonToolbar color="warning">
@@ -41,10 +76,36 @@ const App: React.FC = () => (
     <IonContent className="ion-padding">
       <IonItem>
         <IonLabel position="floating">Your height (meters)</IonLabel>
-        <IonInput></IonInput>
+        <IonInput ref={heightInputRef}></IonInput>
       </IonItem>
+      <IonItem>
+        <IonLabel position="floating">Your weight (kg)</IonLabel>
+        <IonInput ref={weightInputRef}></IonInput>
+      </IonItem>
+      <IonGrid>
+        <IonRow>
+          <IonCol size='12'>
+            <IonButton color="warning" expand="full" shape="round" onClick={calculateBMI}>
+              <IonIcon slot="end" icon={calculator}/>
+              Calculate
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol size='12'>
+            <IonButton color="dark" expand="full" shape="round" fill="outline" onClick={resetInputs}>
+              <IonIcon slot="end" icon={refreshCircleOutline}/>
+              Reset
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol></IonCol>
+        </IonRow>
+      </IonGrid>
     </IonContent>
   </IonApp>
-);
+  )
+};
 
 export default App;
